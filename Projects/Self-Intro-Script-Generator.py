b8b84cc4e55f19df8abc13279082ea749da213d6
Rakesh kumar Jani <rakesh@Rakeshs-MacBook-Air.local>
2025-09-24 02:24:49 +0530

    refactor: update the self intro generator

--- Projects/Self-Intro-Script-Generator.py
+++ Projects/Self-Intro-Script-Generator.py
@@ -1,13 +1,14 @@
 # Self Intro Script Generator
 import datetime
 
-name = input("Can you please tell me your name?")
-age = input("Can you please tell me your age?")
-city = input("Can you please tell me in which city you live?")
-profession = input("What do you do for living?")
-hobby = input("What do like to do in your free time?")
+name = input("Can you please tell me your name?").strip()
+age = input("Can you please tell me your age?").strip()
+city = input("Can you please tell me in which city you live?").strip()
+profession = input("What do you do for living?").strip()
+hobby = input("What do like to do in your free time?").strip()
 
-print("\n *************************************************************************************************************************************************************** \n\n")
+border = "*" * 80
+print("\n {border}  \n\n")
 print(f"Hello! My name is {name}. I'm {age} years old and live in {city}. I work as a {profession} and I absolutely enjoy {hobby} in my free time. Nice to meet you!")
 print("\n *************************************************************************************************************************************************************** \n\n")
 current_date = datetime.date.today().isoformat()

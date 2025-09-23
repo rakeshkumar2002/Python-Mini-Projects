e239649e98f869089727a0144230c39888fef1f8
Rakesh kumar Jani <rakesh@Rakeshs-MacBook-Air.local>
2025-09-24 02:13:14 +0530

    feat: Add self into script generator

--- Projects/Self-Intro-Script-Generator.py
+++ Projects/Self-Intro-Script-Generator.py
@@ -1,25 +1,14 @@
-"""
- Challenge: Self-Intro Script Generator
+# Self Intro Script Generator
+import datetime
 
-Create a Python script that interacts with the user and generates a personalized self-introduction.
+name = input("Can you please tell me your name?")
+age = input("Can you please tell me your age?")
+city = input("Can you please tell me in which city you live?")
+profession = input("What do you do for living?")
+hobby = input("What do like to do in your free time?")
 
-Your program should:
-1. Ask the user for their name, age, city, profession, and favorite hobby.
-2. Format this data into a warm, friendly paragraph of self-introduction.
-3. Print the final paragraph in a clean and readable format.
-
-Example:
-If the user inputs:
-  Name: Priya
-  Age: 22
-  City: Jaipur
-  Profession: Software Developer
-  Hobby: playing guitar
-
-Your script might output:
-  "Hello! My name is Priya. I'm 22 years old and live in Jaipur. I work as a Software Developer and I absolutely enjoy playing guitar in my free time. Nice to meet you!"
-
-Bonus:
-- Add the current date to the end of the paragraph like: "Logged on: 2025-06-14"
-- Wrap the printed message with a decorative border of stars (*)
-"""
+print("\n *************************************************************************************************************************************************************** \n\n")
+print(f"Hello! My name is {name}. I'm {age} years old and live in {city}. I work as a {profession} and I absolutely enjoy {hobby} in my free time. Nice to meet you!")
+print("\n *************************************************************************************************************************************************************** \n\n")
+current_date = datetime.date.today().isoformat()
+print(f"\n Logged on: {current_date} \n")
